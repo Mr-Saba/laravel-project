a020ae9214a1e4a38835581fe704ddde91502c6d
Saba <sabamirzashvili12@gmail.com>
2020-04-04 17:47:41 +0400

    davaleba1

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" />
+    <link rel="stylesheet" href="/css/style.css" />
+    <title></title>
+    <style></style>
+</head>
+
+<body>
+<div class="menu1">
+<span class="blog">Blog</span>
+<span class="template">template</span>
+</div>
+<div class="menu2">
+</div>
+
+</body> 
+
+</html>
