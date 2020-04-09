33b00ce7f758eae9b3da65a95e7592e7e96dd203
Saba <sabamirzashvili12@gmail.com>
2020-04-09 19:58:10 +0400

    mziuri

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" />
-    <link rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="style.css" />
     <title></title>
-    <style></style>
+    <style>
+
+    </style>
 </head>
 
 <body>
@@ -13,8 +15,95 @@
 <span class="template">template</span>
 </div>
 <div class="menu2">
+    <i class="fas fa-search" id="logo"></i>
+    <span class="item1">About</span>
+    <span  class="item1">Cooperation</span>
+    <span  class="item1">Contact</span>
+    <span  class="item3">Nature</span>
+    <span  class="item2">People</span>
+    <span  class="item2">Trips</span>
+    <span  class="item2">Animals</span>
+    <span  class="item2">Fashion</span>
+    <span  class="item2">Tech</span>
+    <span  class="item2">. . .</span>
 </div>
+<img class="main-img" src="/mziuri/img/12.png" />
+<img class="img1" src="/mziuri/img/123.png" />
+<div class="txt1">Maecenas quis lobortis nunc. Nullam sit</div>
+<div class="txt2">vel odio congue vulputate a ut nisi.</div>
+<button class="butt">People</button>
+<div class="line">
+    <img src="/mziuri/img/14.png" class="img2"/>
+    <button class="tech1">Tech</button>
+    <img src="/mziuri/img/15.png" class="img2"/>
+    <button class="tech2">Tech</button>
+</div>
+<div class="line">
+    <p class="msg1">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</p>
+    <p class="msg2">Maecenas quis lobortis nunc. Nullam sit amet augue vel odio congue vulputate a ut nisi.</p>
+</div>
+<span class="date1">11 septemvber 2016  |   John Doe</span>
+<span class="date2">11 septemvber 2016  |   John Doe</span>
+<img src="/mziuri/img/16.png" class="img3"/>
+<div class="big-txt">Proin congue elit fringilla</div>
+<div class="small-txt1">sodales tellus interdum fermentum nulla. Aliquam vitae arcu condimentum,</div>
+<div class="small-txt2">consectetur diam sed, ultrices urna. </div>
+<button class="tech3">Tech</button>
 
+<div class="line1">
+<img src="/mziuri/img/11.png" class="img4">
+<button class="tech4">Tech</button>
+<img src="/mziuri/img/22.png" class="img4">
+<button class="tech5">Tech</button>
+<img src="/mziuri/img/33.png" class="img4">
+<button class="tech6">Tech</button>
+</div>
+<span class="message1">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+<span class="message2">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+<span class="message3">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+<span class="dat1">11 septemvber 2016  |   John Doe</span>
+<span class="dat2">11 septemvber 2016  |   John Doe</span>
+<span class="dat3">11 septemvber 2016  |   John Doe</span>
+
+<div class="line1">
+<img src="/mziuri/img/44.png" class="img4">
+<button class="tech7">Tech</button>
+<img src="/mziuri/img/55.png" class="img4">
+<button class="tech8">Tech</button>
+<img src="/mziuri/img/66.png" class="img4">
+<button class="tech9">Tech</button> 
+</div>
+<span class="message1">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+<span class="message2">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+<span class="message3">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+<span class="dat1">11 septemvber 2016  |   John Doe</span>
+<span class="dat2">11 septemvber 2016  |   John Doe</span>
+<span class="dat3">11 septemvber 2016  |   John Doe</span>
+<br><br><br>
+<div class="line">
+    <img src="/mziuri/img/14.png" class="img2"/>
+    <button class="tech1">Tech</button>
+    <img src="/mziuri/img/15.png" class="img2"/>
+    <button class="tech2">Tech</button>
+</div>
+<div class="line">
+    <p class="msg1">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</p>
+    <p class="msg2">Maecenas quis lobortis nunc. Nullam sit amet augue vel odio congue vulputate a ut nisi.</p>
+</div>
+<span class="date1">11 septemvber 2016  |   John Doe</span>
+<span class="date2">11 septemvber 2016  |   John Doe</span>
+
+<div class="last-1">
+    <div class="last-big">Newsletter</div>
+    <div class="last-small">Donec elementum dui semper, pretium dui quis, pretium nisl.</div>
+    <div class="last-background">
+        <div class="last-button"><p class="last-button-txt">Sign up</p></div>
+    </div>
+</div>
+<div class="last-2">
+ <div class="last-small2" >Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</div>
+    <div class="last-small3" >Try awsome tool for desgners symu.co</div>
+</div>
 </body> 
 
 </html>
