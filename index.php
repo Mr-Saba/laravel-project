6ece748269b6e127c830cb7ba6497f648cb1e407
Saba <sabamirzashvili12@gmail.com>
2020-04-16 21:30:09 +0400

    shecvla

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" />
     <link rel="stylesheet" href="style.css" />
     <title></title>
@@ -10,100 +11,156 @@
 </head>
 
 <body>
-<nav class="flex">
- <div class="menu1" >
-     <span class="menu1-text" >template</span>
-     <span class="menu1-text bold">Blog</span>
- </div>
- <div class="menu2">
- <i class="fas fa-search logo"></i>
- <ul class="menu2-text1">
-     <div class="item1">About</div>
-     <div class="item1">Cooperation</div>
-     <div class="item1">Contact</div>
-     <div class="item2 space">Nature</div>
-     <div class="item2">People</div>
-     <div class="item2">Trips</div>
-     <div class="item2">Animals</div>
-     <div class="item2">Fashion</div>
-     <div class="item2">Tech</div>
-     <div class="item2 bold">. . .</div>
- </ul>
-</div>
-</nav>
-<img class="main-img" src="/mziuri/img/12.png" />
-<img class="img1" src="/mziuri/img/123.png" />
-<div class="txt1">Maecenas quis lobortis nunc. Nullam sit</div>
-<div class="txt2">vel odio congue vulputate a ut nisi.</div>
-<button class="butt">People</button>
-<div class="container"> 
-    <img src="/mziuri/img/14.png" class="photo"/>
-    <img src="/mziuri/img/15.png" class="photo"/>
-    <p class="msg1">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</p>
-    <p class="msg2">Maecenas quis lobortis nunc. Nullam sit amet augue vel odio congue vulputate a ut nisi.</p>
-</div>
-<span class="date1">11 septemvber 2016  |   John Doe</span>
-<span class="date2">11 septemvber 2016  |   John Doe</span>
-<img src="/mziuri/img/16.png" class="img3"/>
-<div class="big-txt">Proin congue elit fringilla</div>
-<div class="small-txt1">sodales tellus interdum fermentum nulla. Aliquam vitae arcu condimentum,</div>
-<div class="small-txt2">consectetur diam sed, ultrices urna. </div>
-<button class="tech3">Tech</button>
-
-<div class="line1">
-<img src="/mziuri/img/11.png" class="img4">
-<button class="tech4">Tech</button>
-<img src="/mziuri/img/22.png" class="img4">
-<button class="tech5">Tech</button>
-<img src="/mziuri/img/33.png" class="img4">
-<button class="tech6">Tech</button>
-</div>
-<span class="message1">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
-<span class="message2">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
-<span class="message3">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
-<span class="dat1">11 septemvber 2016  |   John Doe</span>
-<span class="dat2">11 septemvber 2016  |   John Doe</span>
-<span class="dat3">11 septemvber 2016  |   John Doe</span>
-
-<div class="line1">
-<img src="/mziuri/img/44.png" class="img4">
-<button class="tech7">Tech</button>
-<img src="/mziuri/img/55.png" class="img4">
-<button class="tech8">Tech</button>
-<img src="/mziuri/img/66.png" class="img4">
-<button class="tech9">Tech</button> 
-</div>
-<span class="message1">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
-<span class="message2">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
-<span class="message3">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
-<span class="dat1">11 septemvber 2016  |   John Doe</span>
-<span class="dat2">11 septemvber 2016  |   John Doe</span>
-<span class="dat3">11 septemvber 2016  |   John Doe</span>
-<br><br><br>
-<div class="line">
-    <img src="/mziuri/img/14.png" class="img2"/>
-    <button class="tech1">Tech</button>
-    <img src="/mziuri/img/15.png" class="img2"/>
-    <button class="tech2">Tech</button>
-</div>
-<div class="line">
-    <p class="msg1">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</p>
-    <p class="msg2">Maecenas quis lobortis nunc. Nullam sit amet augue vel odio congue vulputate a ut nisi.</p>
-</div>
-<span class="date1">11 septemvber 2016  |   John Doe</span>
-<span class="date2">11 septemvber 2016  |   John Doe</span>
-
-<div class="last-1">
-    <div class="last-big">Newsletter</div>
-    <div class="last-small">Donec elementum dui semper, pretium dui quis, pretium nisl.</div>
-    <div class="last-background">
-        <div class="last-button"><p class="last-button-txt">Sign up</p></div>
+    <nav class="flex">
+        <div class="menu1">
+            <span class="menu1-text">template</span>
+            <span class="menu1-text bold">Blog</span>
+        </div>
+        <div class="menu2">
+            <i class="fas fa-search logo"></i>
+            <ul class="menu2-text1">
+                <div class="item1">About</div>
+                <div class="item1">Cooperation</div>
+                <div class="item1">Contact</div>
+                <div class="item2 space">Nature</div>
+                <div class="item2">People</div>
+                <div class="item2">Trips</div>
+                <div class="item2">Animals</div>
+                <div class="item2">Fashion</div>
+                <div class="item2">Tech</div>
+                <div class="item2 bold">. . .</div>
+            </ul>
+        </div>
+    </nav>
+    <div class="post-image">
+        <img class="main-img" src="/mziuri/img/12.png" />
+        <img class="img1" src="/mziuri/img/123.png" />
+        <div class="txt1">Maecenas quis lobortis nunc. Nullam sit</div>
+        <div class="txt2">vel odio congue vulputate a ut nisi.</div>
+        <button class="butt">People</button>
     </div>
-</div>
-<div class="last-2">
- <div class="last-small2" >Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</div>
-    <div class="last-small3" >Try awsome tool for desgners symu.co</div>
-</div>
-</body> 
+    <div class="container">
+        <div class="post">
+            <div class="post-image">
+                <img src="/mziuri/img/14.png" class="photo" />
+                <span class="post-tag">Trips</span>
+            </div>
+            <div class="text">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</div>
+            <div class="date">11 septemvber 2016 | John Doe</div>
+        </div>
+        <div class="post">
+            <div class="post-image">
+                <img src="/mziuri/img/15.png" class="photo" />
+                <span class="post-tag">Tech</span>
+            </div>
+            <div class="text">Maecenas quis lobortis nunc. Nullam sit amet augue vel odio congue vulputate a ut nisi.
+            </div>
+            <div class="date">11 septemvber 2016 | John Doe</div>
+        </div>
+        <div class="post-image">
+            <img src="/mziuri/img/16.png" class="big-photo" />
+            <div class="big-txt">Proin congue elit fringilla</div>
+            <div class="small-txt">sodales tellus interdum fermentum nulla. Aliquam vitae arcu condimentum, consectetur
+                diam sed, ultrices urna. </div>
+            <span class="button1">Trips</span>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="post1">
+            <div class="post-image">
+                <img src="/mziuri/img/11.png" class="photo">
+                <span class="post-tag">Nature</span>
+            </div>
+            <div class="text">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</div>
+            <div class="date1">11 septemvber 2016 | John Doe</div>
+        </div>
+        <div class="post1">
+            <div class="post-image">
+                <img src="/mziuri/img/22.png" class="photo">
+                <span class="post-tag">People</span>
+            </div>
+            <div class="text">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</div>
+            <div class="date1">11 septemvber 2016 | John Doe</div>
+        </div>
+        <div class="post1">
+        <div class="post-image">
+        <img src="/mziuri/img/33.png" class="photo">
+        <span class="post-tag">Tech</span>
+    </div>
+    <div class="text">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</div>
+            <div class="date1">11 septemvber 2016 | John Doe</div>
+        </div>
+    </div>
+        <div class="container">
+        <div class="post1">
+            <div class="post-image">
+        <img src="/mziuri/img/44.png" class="photo">
+        <span class="post-tag">Trips</span>
+            </div>
+            <div class="text">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</div>
+            <div class="date1">11 septemvber 2016 | John Doe</div>
+        </div>
+        <div class="post1">
+            <div class="post-image">
+        <img src="/mziuri/img/55.png" class="photo">
+        <span class="post-tag">Trips</span>
+            </div>
+            <div class="text">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</div>
+            <div class="date1">11 septemvber 2016 | John Doe</div>
+        </div>
+        <div class="post1">
+            <div class="post-image">
+        <img src="/mziuri/img/66.png" class="photo">
+        <span class="post-tag">Fashion</span>
+            </div>
+            <div class="text">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</div>
+            <div class="date1">11 septemvber 2016 | John Doe</div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="post">
+            <div class="post-image">
+                <img src="/mziuri/img/14.png" class="photo" />
+                <span class="post-tag">Trips</span>
+            </div>
+            <div class="text">Donec elementum dui semper, pretium dui quis, pre- tium nisl. Nunc quis ornare odio.</div>
+            <div class="date">11 septemvber 2016 | John Doe</div>
+        </div>
+        <div class="post">
+            <div class="post-image">
+                <img src="/mziuri/img/15.png" class="photo" />
+                <span class="post-tag">Tech</span>
+            </div>
+            <div class="text">Maecenas quis lobortis nunc. Nullam sit amet augue vel odio congue vulputate a ut nisi.
+            </div>
+            <div class="date">11 septemvber 2016 | John Doe</div>
+        </div>
+
+    </div>
+    <div class="last-1">
+        <div class="last-big">Newsletter</div>
+        <div class="last-small">Donec elementum dui semper, pretium dui quis, pretium nisl.</div>
+        <div class="last-background">
+            <div class="last-button">
+                <p class="last-button-txt">Sign up</p>
+            </div>
+        </div>
+    </div>
+    <div class="last-2">
+        <div class="last-small2">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        </div>
+        <div class="last-small3">Try awsome tool for desgners symu.co</div>
+    </div>
+    <!-- <?php 
+    $dbh = new PDO('mysql:host=localhost;dbname=baza', 'root'); 
+    $statement = $dbh->prepare('SELECT * FROM baza LIMIT 10');
+    $statement->execute();
+    $data = $statement->fetchAll();
+    echo  '<pre>';
+       print_r($data);
+    echo  '</pre>';
+    ?> -->
+</body>
 
 </html>
